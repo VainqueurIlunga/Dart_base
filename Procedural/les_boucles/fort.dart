ba861eb32188dc1b8470db16406f450ccaed4a22
VainqueurIlunga <ilungafranck236@gmail.com>
2023-08-20 21:53:09 +0200

    la structure repetitive ou iterative

---
+++ Procedural/les_boucles/fort.dart
@@ -0,0 +1,14 @@
+//Les boucles permettent d'exécuter un bloc de code plusieurs fois,
+// en fonction d'une condition.
+// En Dart, les boucles les plus courantes sont les suivantes:
+// for :Utilisé pour itérer un nombre spécifique de fois.
+
+main() {
+  int nbr=5;
+  for (int j = 1; j <=nbr; j++)
+  {
+    print("je vais tourner 5 fois");
+  }
+
+  
+}
---
+++ Procedural/les_boucles/while.dart
@@ -0,0 +1,15 @@
+//Les boucles permettent d'exécuter un bloc de code plusieurs fois,
+// en fonction d'une condition.
+// En Dart, les boucles les plus courantes sont les suivantes:
+// for
+//while: Utilisé pour itérer tant qu'une condition est vraie.
+
+void main() {
+  int x = 1;
+  while (x != 5){
+    print(x);
+    x++;
+}
+print("La valeur final de x est $x");
+  
+}
