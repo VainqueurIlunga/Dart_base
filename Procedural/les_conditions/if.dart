3793c2af629dde5b358a4e4d5fc7cffb90500d84
Vainqueur  ilunga <112663514+VainqueurIlunga@users.noreply.github.com>
2024-07-09 16:42:07 +0200

    Update if.dart

--- Procedural/les_conditions/if.dart
+++ Procedural/les_conditions/if.dart
@@ -1,6 +1,6 @@
 // l'instruction if pour si en français permet au programme de faire quelque chose uniquement si 
 //une certaine condition est vraie
-// si une condition est remplie, le code du bloc if sera eecuter, par contre si la condition est fausse
+// si une condition est remplie, le code du bloc if sera executer, par contre si la condition est fausse
 //le code du bloc if ne sera pas executer
 void main() {
   int age = 15;
