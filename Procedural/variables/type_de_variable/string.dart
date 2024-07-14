d1d20bf70064e205d4aa197bc71ef1ac4e9054f4
Vainqueur Ilunga <ilungafranck236@gmail.com>
2024-07-15 00:15:29 +0200

    suite de la formation dart, type de variable string

---
+++ Procedural/variables/type_de_variable/string.dart
@@ -0,0 +1,9 @@
+void main(){
+    // string est un type de variable qui prend de lettre de a à z, 
+    //de chiffre de 0 à 9, et de caracter special telle que @, # ...
+    // les valeur de string sont entre le double ou simple cote '' ou "", 
+    //voici la synthax pour declarer une variable de type dart
+    var fistname = "Vainqueur";
+    print(fistname);
+
+}
